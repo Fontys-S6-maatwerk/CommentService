ba45b533c38410a607f19c25b4d277b60e5562db
Heuver,Maxime M.M.J <maxime.heuver@gmail.com>
2021-10-13 11:58:34 +0200

    added empty classes and added get all endpoint

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,24 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CommentService.Models;
+using CommentService.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 
 namespace CommentService.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/comments")]
     public class CommentController : ControllerBase
     {
         private readonly ILogger<CommentController> _logger;
+        private IRepository _repository;
 
-        public CommentController(ILogger<CommentController> logger)
+        public CommentController(ILogger<CommentController> logger, IRepository repository)
         {
+            _repository = repository;
             _logger = logger;
         }
 
-        [HttpGet("all")]
+        [HttpGet]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(CommentQueryModel[]), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public IActionResult GetComments([FromQuery] int pageSize, [FromQuery] int sectionNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+            [HttpGet("all")]
         public IEnumerable<Comment> All()
         {
             return Enumerable.Range(1, 5).Select(index => new Comment
---
+++ Repositories/DbContextRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommentService.Repositories
+{
+    public class DbContextRepository
+    {
+    }
+}
