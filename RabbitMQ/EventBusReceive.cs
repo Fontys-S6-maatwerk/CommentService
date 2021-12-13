852da392d1dfa40d92d61c23bd7a43377c0112b0
Ella Zuidema <ella.zuidema@student.fontys.nl>
2021-12-13 15:55:55 +0100

    fixed rabbitmq

--- RabbitMQ/EventBusReceive.cs
+++ RabbitMQ/EventBusReceive.cs
@@ -29,51 +29,42 @@ namespace CommentService.RabbitMQ
 
         public void ReceiveUser()
         {
-            var queueName = "auth_comment_queue";
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            connection = factory.CreateConnection();
-            channel = connection.CreateModel();
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "auth_logs", type: ExchangeType.Fanout);
 
-            channel.ExchangeDeclare(exchange: "topic_logs", ExchangeType.Fanout);
-            channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
-            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "auth_logs",
+                              routingKey: "");
 
-            channel.QueueBind(queue: queueName, exchange: "topic_logs", routingKey: "auth");
-
-            Console.WriteLine(" [*] Waiting for messages.");
+            Console.WriteLine(" [*] Waiting for logs.");
 
             var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (sender, ea) =>
+            consumer.Received += (model, ea) =>
             {
                 UserQM response = new UserQM();
-
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine(" [x] Received {0}", message);
-
                 int dots = message.Split('.').Length - 1;
-
-                //Do something with the message.
                 try
                 {
-                    Console.WriteLine(" [.] User({0})", message);
+                    Console.WriteLine("[.] User({0})", message);
                     response = JsonConvert.DeserializeObject<UserQM>(message);
 
-                    //TODO save changes in database
                     _repository.AddUser(CommentMapper.MapToUserDBO(response));
                 }
-                catch (Exception e)
+                catch(Exception e)
                 {
-                    Console.WriteLine(" [.] " + e.Message);
-                    response = null;
+                    Console.WriteLine("[.] " + e.Message);
                 }
-
-                Console.WriteLine(" [x] Done " + response.FirstName);
-                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
             channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
 
             Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,7 @@ namespace CommentService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CommentContext dataContext)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CommentContext dataContext, EventBusReceive cake)
         {
             foreach (var migration in dataContext.Database.GetPendingMigrations())
             {
